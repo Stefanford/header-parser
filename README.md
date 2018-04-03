0ed02662d11c8589688910b482c9e49f7e59798e
Stefan Savic <stefanford1985@gmail.com>
2018-04-03 16:22:25 +0200

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # header-parser
+Free code camp project Header-parser
---
+++ header-parser.js
@@ -0,0 +1,35 @@
+var expres = require('express');
+var bodyParser = require('body-parser');
+var cors  = require('cors');
+var get_ip = require('ipware')().get_ip;
+var userAgent = require('express-useragent');
+
+
+
+var project = module.exports = expres();
+project.use(bodyParser.json());
+project.use(cors());
+project.use(userAgent.express());
+
+var apiUrl = "/api/whoami";
+
+
+
+project.get(apiUrl, function(req,res,next){
+
+    var systemInfo = {
+        ipaddress: get_ip(req).clientIp,
+        language: (req.acceptsLanguages())[0],
+        software: req.useragent.os
+        }
+
+    res.json({
+        "ipaddress": systemInfo.ipaddress,
+        "language": systemInfo.language,
+        "software": systemInfo.software
+    });
+});
+
+project.listen(8000, function(){
+    console.log('Node is working');
+})
